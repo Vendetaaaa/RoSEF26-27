6b683d839a0b2bf67d1d3934be1cb4194fbc689e
Vendeta <VZVendeta@hotmail.com>
2026-09-22 17:52:51 +0300

    commit error 🥹

--- informatica/lattice/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Reusable HNP lattice helpers for the RoSEF experiments."""
-
-from .solver import HNPConfig, HNPLatticeSolver, build_hnp_inputs
-
-__all__ = ["HNPConfig", "HNPLatticeSolver", "build_hnp_inputs"]
